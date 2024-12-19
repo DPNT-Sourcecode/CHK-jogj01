cef8087057aa04bfef79b7cfef80443127baa094
sbx_user1051 <sbx_user1051@169.254.9.186>
2024-12-19 19:03:20 +0000

    Thu Dec 19 19:03:20 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -20,8 +20,6 @@ def checkout(skus):
                 basket[sku] += 1
             else:
                 basket[sku] = 1
-        
-        print(basket)
 
         for sku,quantity in basket.items():
             if not sku in offers:
@@ -30,11 +28,9 @@ def checkout(skus):
                 num_single_items = basket[sku] % offers[sku]['q']
                 num_bundles = int(basket[sku] / offers[sku]['q'])
                 total += (num_single_items * products[sku] + num_bundles * offers[sku]['p'])
-        print(total)
         return total
 
-
-    return -1
+    # return -1
 
 
 if __name__ == "__main__":
@@ -42,6 +38,7 @@ if __name__ == "__main__":
 
     skus = sys.argv[1]
     checkout(skus)
+
 
 
 
