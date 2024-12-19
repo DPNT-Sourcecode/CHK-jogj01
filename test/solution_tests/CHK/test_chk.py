21eebefb776906259e658eb68d3325cc521bf682
sbx_user1051 <sbx_user1051@169.254.9.186>
2024-12-19 19:23:06 +0000

    Thu Dec 19 19:23:06 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -7,14 +7,17 @@ import pytest
     ("B", 30),
     ("C", 20),
     ("D", 15),
-    ("", -1),
     ("Z", -1),
     (100, -1),
+    ("", 0),
     ("AA", 100),
     ("AABCDBAACDDD", 345),
     ("AABCDBA", 230),
-    ("AABZTCDBA", 230),
+    ("AABZTCDBA", -1),
+    ("ABCa", -1),
+    ("AxA", -1),
 
 ])
 def test_checkout(skus, expected_result):
     assert checkout_solution.checkout(skus) == expected_result
+
