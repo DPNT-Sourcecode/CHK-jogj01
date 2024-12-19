e9ea9bfff1fb3b881724ef4b920d9956a6a93860
sbx_user1051 <sbx_user1051@169.254.9.186>
2024-12-19 14:27:22 +0000

    Thu Dec 19 14:27:22 UTC 2024

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -1,7 +1,11 @@
 from lib.solutions.HLO import hello_solution
+import pytest
 
 
-class TestHello():
-    def test_hello(self):
-        assert hello_solution.hello("Robert") == "Hello Robert!"
-        assert hello_solution.hello("") == "Please enter your friend's name."
+@pytest.mark.parametrize("friend_name, expected_result", [
+    ("Robert", "Hello Robert!"),
+    ("", "Please enter your friend's name."),
+    ("1955489", "Please enter your friend's name."),
+])
+def test_hello(friend_name, expected_result):
+    assert hello_solution.hello(friend_name) == expected_result
